6779dbf96fe4c1f9ef5a459e4daa8e19e0a8a7f4
Muhammad Usama <usama@Muhammads-MacBook-Pro.local>
2023-11-18 22:16:16 +0500

    added custom validation

--- test/test.js
+++ test/test.js
@@ -2,7 +2,19 @@
 const PasswordStrengthChecker = require('../lib/index');
 
 // Example usage
-const password = 'StrongP@ssw0rd';
+const password = ' s ';
 const strength = PasswordStrengthChecker.checkPasswordStrength(password);
 
 console.log(`Password strength: ${strength}`);
+
+// Custom usage
+const options = {
+length: 1,
+uppercase: true,
+lowercase: true,
+number: true,
+special: true,
+trimmed: true
+}
+const isPasswordValid = PasswordStrengthChecker.customPasswordVerification(password, options)
+console.log(`Password Valid: ${isPasswordValid}`);
